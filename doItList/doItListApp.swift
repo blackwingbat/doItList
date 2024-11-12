922c87d7612b7bc7f38c376c712b2c6fad67ca40
blackwingbat <82639942+blackwingbat@users.noreply.github.com>
2024-11-12 23:08:33 +0700

    Initial Commit

---
+++ doItList/doItListApp.swift
@@ -0,0 +1,17 @@
+//
+//  doItListApp.swift
+//  doItList
+//
+//  Created by Ahmad Fadel on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct doItListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
