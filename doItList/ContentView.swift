7d0a6ee01e64e6943c3729fa3f74b2292d0eec43
blackwingbat <82639942+blackwingbat@users.noreply.github.com>
2024-11-14 22:55:57 +0700

    Update ContentView.swift

--- doItList/ContentView.swift
+++ doItList/ContentView.swift
@@ -8,16 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tasks: [Task]=[]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(tasks) { task in
+                    TaskRow(task: task)
+                }
+                .onDelete(perform: deleteTask)
+            }
+            .navigationBarTitle("Do-It List")
+            .navigationBarItems(trailing:
+                                    Button(action: addTask){
+                Image(systemName:"plus")
+            }
+            )
         }
-        .padding()
+        
+    }
+    func deleteTask(at offsets: IndexSet){
+        tasks.remove(atOffsets: offsets)
+    }
+    
+    func addTask(){
+        
+    }
+    
+}
+
+struct Task: Identifiable{
+    let id = UUID()
+    var title: String
+    var description: String
+    var priority: String
+    var isCompleted: Bool
+}
+
+struct TaskRow: View{
+    var task: Task
+    
+    var body: some View{
+        HStack{
+            Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle").onTapGesture {
+                
+            }
+            VStack(alignment: .leading){
+                Text(task.title)
+                    .font(.headline)
+                Text(task.priority)
+                    .font(.subheadline)
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
